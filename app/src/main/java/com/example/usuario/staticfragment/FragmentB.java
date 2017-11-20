7d26e53b8c6f3a0c298b11e3074f288b6cf9c6e5
zhuoIC <nicolimonar@gmail.com>
2017-11-20 11:08:24 +0100

    Solucion error al girar: usar la librería Fragment y no la de support.

--- app/src/main/java/com/example/usuario/staticfragment/FragmentB.java
+++ app/src/main/java/com/example/usuario/staticfragment/FragmentB.java
@@ -1,9 +1,9 @@
 package com.example.usuario.staticfragment;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
